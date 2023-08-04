144750c2a6e91331759d13175561b24f04a982ab
GuilhermeLucioChaves <130411237+GuilhermeLucioChaves@users.noreply.github.com>
2023-08-04 17:13:32 -0300

    novo src

---
+++ src/Componentes/Header/Index.js
@@ -0,0 +1,16 @@
+import './Index.css'
+
+const Header =()=>{
+    return(
+        <header className='Header'> 
+            <h2>Vcar</h2>
+            <div className="conteiner-nav">
+                <a src="*" className="nav">Home</a>
+                <a src="*" className="nav">Serviços</a>
+                <a src="*" className="nav">Contato</a>
+            </div>
+        </header>
+    )
+}
+
+export default Header
---
+++ src/Componentes/InicalPage/Index.js
@@ -0,0 +1,13 @@
+import './Index.css'
+
+const MainPage= () =>{
+    return(
+        <body>
+            <h1 className='body-slogan'> Seu Carro sempre Limpo</h1>
+            <img src='*' alt='Carro do caralho'></img>
+            <a> Agende já</a>
+        </body>
+    )
+ }
+
+ export default MainPage
